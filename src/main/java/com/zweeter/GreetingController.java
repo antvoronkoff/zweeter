1244f2c01603ccd98ae90e049ef49fc8b64bed57
Anton <antvoronkoff@gmaill.com>
2021-01-02 21:13:21 +0300

    Replace thymeleaf ->mustache

--- src/main/java/com/zweeter/GreetingController.java
+++ src/main/java/com/zweeter/GreetingController.java
@@ -1,7 +1,7 @@
 package com.zweeter;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
         return "greeting";
     }
 
